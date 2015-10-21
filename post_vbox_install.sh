4ca36aeee67ffae5a042dd575d80db8a8e9f3f74
Joshua Phipps <josh.e.phipps@gmail.com>
2015-10-21 12:25:05 -0400

    Add next file

--- post_vbox_install.sh
+++ post_vbox_install.sh
@@ -1,7 +1,18 @@
+# Example: bash post_vbox_install.sh torrent /mnt/torrents/
+
 echo "Must have installed VBox Guest Additions"
 
 sudo mkdir -p $2
 
-sudo mount -t vboxsf -0 uid=1000,gid=1000 $1 $2
+sudo mount -t vboxsf -o uid=1000,gid=1000 $1 $2
 
-sudo printf '%s\t%s\tvboxsf\tuid=1000,gid=1000\t0\t0' $1 $2 >> /etc/fstab
+echo
+echo "Copy the following line to add to /etc/fstab"
+echo
+printf '%s\t%s\tvboxsf\tuid=1000,gid=1000\t0\t0' $1 $2
+
+echo
+echo
+echo "Press any key to enter /etc/fstab"
+read
+sudo nano /etc/fstab
