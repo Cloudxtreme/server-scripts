f4cd03a4f15509e28c40cdcc5f50ec8463dea817
Joshua Phipps <josh.e.phipps@gmail.com>
2015-10-22 09:52:10 -0400

    Change chg_hostname.sh

--- chg_hostname.sh
+++ chg_hostname.sh
@@ -1,11 +1,25 @@
-printf "\n%s\n%s" "1. Edit the hostname in /etc/hostname." "  - Press key to edit.."
-read
-sudo nano /etc/hostname
+#!/bin/bash
 
-printf "\n%s\n%s" "2. Edit the hostname for 127.0.0.1 in /etc/hosts." "  - Press key to edit.."
-read
-sudo nano /etc/hosts
 
-printf "\n%s" "3. Press key to reboot now.."
-read
-sudo reboot now
+thishost=$(hostname)
+printf '\n%s %s\n%s' 'Current Hostname:' $thishost 'Would you like to change it? (y/n) : '
+read ans
+
+
+if [ $ans == 'y' ] || [ $ans == 'Y' ]; then
+    printf "\n%s\n%s" "Edit the hostname in /etc/hostname." "  - Press key to edit.."
+    read
+    sudo nano /etc/hostname
+
+    printf "\n%s\n%s" "Edit the hostname for 127.0.0.1 in /etc/hosts." "  - Press key to edit.."
+    read
+    sudo nano /etc/hosts
+
+    printf "\n%s" "Would you like to reboot now? (y/n) : "
+    read ans2
+    if [ $ans2 == 'y' ] || [ $ans2 == 'Y' ]; then
+        sudo reboot now
+    else
+        printf "\n%s\n" "This change will not take effect until you reboot!"
+    fi
+fi
